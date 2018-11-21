dca3af80e6d23ecc10538dec207330569b39e3f0
John Casabar <johncasabar@gmail.com>
2018-11-21 04:01:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|e| e.name == item_name}
 
-      if item = nil
+      if item == nil
         resp.write "Item not found"
         resp.status = 400
       else
