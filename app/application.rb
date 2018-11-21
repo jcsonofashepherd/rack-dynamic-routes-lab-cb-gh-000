9ba1fd80c15166a1b2ea5cc63e82370cda896369
John Casabar <johncasabar@gmail.com>
2018-11-21 03:55:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items"
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|e| e.name == item_name}
 
